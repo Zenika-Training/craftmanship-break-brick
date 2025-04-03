bbbebe16b3243d275142871c3795a6dd0b59c9a6
Gweltaz Niquel <gweltaz.niquel@ext.bpifrance.fr>
2025-04-03 10:32:30 +0200

    :sparkles: tests: illustrate with samples workbook 2 - doublures de test

--- src/test/java/com/zenika/logic/BricksLogicTest.java
+++ src/test/java/com/zenika/logic/BricksLogicTest.java
@@ -13,6 +13,34 @@ public class BricksLogicTest {
     private final BricksLogic bricksLogic = new BricksLogic(panelWidth);
 
     @Test
+    @DisplayName("Should initialize bricks correctly")
+    void should_initialize_bricks_correctly() {
+        // Arrange
+        int expectedBrickCount = 7;
+
+        // Act
+        int actualBrickCount = bricksLogic.getBricks().size();
+
+        // Assert
+        assertEquals(expectedBrickCount, actualBrickCount);
+    }
+
+    @Test
+    @DisplayName("Should initialize bricks with correct dimensions")
+    void should_initialize_bricks_with_correct_dimensions() {
+        // Given
+        int expectedBrickWidth = 60;
+        int expectedBrickHeight = 20;
+
+        // When
+        Rectangle firstBrick = bricksLogic.getBricks().getFirst();
+
+        // Then
+        assertEquals(expectedBrickWidth, firstBrick.width);
+        assertEquals(expectedBrickHeight, firstBrick.height);
+    }
+
+    @Test
     @DisplayName("Should detect collision and remove brick when ball hits")
     void should_detect_collision_and_remove_brick_when_ball_hits() {
         // Given
@@ -22,7 +50,7 @@ public class BricksLogicTest {
         // When
         boolean collisionDetected = bricksLogic.checkCollision(ball);
 
-        // Then
+        // Then TODO: Use assertCollision method to factorize this test
         assertTrue(collisionDetected);
         assertEquals(initialBrickCount - 1, bricksLogic.getBricks().size());
     }
@@ -37,8 +65,19 @@ public class BricksLogicTest {
         // When
         boolean collisionDetected = bricksLogic.checkCollision(ball);
 
-        // Then
+        // Then TODO: Use assertCollision method to factorize this test
         assertFalse(collisionDetected);
         assertEquals(initialBrickCount, bricksLogic.getBricks().size());
+    }
+
+    private void assertCollision(Rectangle ball, boolean expectedCollision, int expectedBrickCount) {
+        int initialBrickCount = bricksLogic.getBricks().size();
+
+        // When
+        boolean collisionDetected = bricksLogic.checkCollision(ball);
+
+        // Then
+        assertEquals(expectedCollision, collisionDetected);
+        assertEquals(expectedBrickCount, bricksLogic.getBricks().size());
     }
 }
